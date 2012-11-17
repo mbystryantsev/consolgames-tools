fa885f23f732d48d7c10f945f3f68fecf40639c8
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2012-11-17 20:24:05 +0000

    MP3C: Patcher: Added warning for non-production patcher builds

--- game-specific/Metroid Prime 3/Patcher/wizard/IntroPage.cpp
+++ game-specific/Metroid Prime 3/Patcher/wizard/IntroPage.cpp
@@ -3,6 +3,9 @@
 
 IntroPage::IntroPage() : Page()
 {
+#if !defined(PRODUCTION)
+	m_ui.introLabel->setText(m_ui.introLabel->text() + QString::fromLocal8Bit("\n\nдюммюъ бепяхъ оюрвю опедмюгмювемю рнкэйн дкъ бмсрпеммецн хяонкэгнбюмхъ х реярхпнбюмхъ оепебндю!"));
+#endif
 }
 
 int IntroPage::nextId() const 
