a0585436b16fe2c06f63bdd5a20b456601bb521e
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2012-11-18 09:55:56 +0000

    MP3C: Patcher: Added version info

---
+++ game-specific/Metroid Prime 3/Patcher/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0\0"
+
+#define VER_PRODUCTVERSION          1,0,0,0
+#define VER_PRODUCTVERSION_STR      "1.0.0.0\0"
