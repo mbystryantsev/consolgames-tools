2d5f7705edf0a3aa9a20708389b5f26661be993d
horror_x <horror_x@02863142-2508-4e74-98d3-8ef0514c0463>
2012-11-18 19:09:50 +0000

    MP3C: Patcher: Added new version info parts

--- trunk/game-specific/Metroid Prime 3/Patcher/version.h
+++ trunk/game-specific/Metroid Prime 3/Patcher/version.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#define VER_MAJOR 1
+#define VER_MINOR 0
+#define BUILD_MAJOR 0
+#define BUILD_MINOR 0
+
 #define VER_FILEVERSION             1,0,0,0
 #define VER_FILEVERSION_STR         "1.0.0.0\0"
 
