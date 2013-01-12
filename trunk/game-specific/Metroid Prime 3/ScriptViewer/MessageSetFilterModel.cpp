cf417659ba240fa7993f17a3878200a525639602
horror_x <horror_x@02863142-2508-4e74-98d3-8ef0514c0463>
2013-01-12 21:39:10 +0000

    MP3C: ScriptViewer: Improved search - added search by hash or by hash/index in format [hash] or [hash:index]

--- trunk/game-specific/Metroid Prime 3/ScriptViewer/MessageSetFilterModel.cpp
+++ trunk/game-specific/Metroid Prime 3/ScriptViewer/MessageSetFilterModel.cpp
@@ -1,10 +1,13 @@
 #include "MessageSetFilterModel.h"
 #include "MessageSetModel.h"
+#include <QStringList>
 
 MessageSetFilterModel::MessageSetFilterModel()
 	: QSortFilterProxyModel()
 	, m_patternAtBegin(false)
 	, m_patternAtEnd(false)
+	, m_filterHash(0)
+	, m_filterIndex(-1)
 {
 }
 
@@ -23,6 +26,30 @@ void MessageSetFilterModel::setPattern(const QString& pattern)
 	{
 		m_patternAtEnd = true;
 		m_pattern.truncate(m_pattern.size() - 1);
+	}
+
+	bool isIdPointer = false;
+	m_filterHash = 0;
+	m_filterIndex = -1;
+	if (!m_patternAtBegin && !m_patternAtEnd && pattern.startsWith('[') && pattern.endsWith(']'))
+	{
+		QString newPattern = m_pattern.right(m_pattern.size() - 1);
+		newPattern.truncate(newPattern.size() - 1);
+		const QStringList idData = newPattern.split(':');
+		if (idData.size() == 1 || idData.size() == 2)
+		{
+			m_filterHash = idData[0].toULongLong(&isIdPointer, 16);
+			if (isIdPointer && idData.size() == 2)
+			{
+				m_filterIndex = idData[1].toInt(&isIdPointer);
+			}
+		}
+	}
+
+	if (!isIdPointer)
+	{
+		m_filterHash = 0;
+		m_filterIndex = -1;
 	}
 	invalidateFilter();
 }
@@ -54,8 +81,22 @@ bool MessageSetFilterModel::filterAcceptsRow(int sourceRow, const QModelIndex& s
 	}
 
 	const MessageSetModel& model = dynamic_cast<const MessageSetModel&>(*sourceModel());
-	const QString& text = model.messages()[sourceParent.row()].messages[sourceRow].text;
+	const MessageSet& messageSet = model.messages()[sourceParent.row()];
+	if (m_filterHash != 0)
+	{
+		if (!messageSet.nameHashes.contains(m_filterHash))
+		{
+			return false;
+		}
+		if (m_filterIndex >= 0 && m_filterIndex != sourceRow)
+		{
+			return false;
+		}
+		return true;
+	}
 
+
+	const QString& text = messageSet.messages[sourceRow].text;
 	if (stringSatisfyFilter(text))
 	{
 		return true;
