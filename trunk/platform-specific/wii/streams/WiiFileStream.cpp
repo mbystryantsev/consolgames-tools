ccbe58e2e1011d9925e2ee03c86266e1ad770407
horror_x <horror_x@02863142-2508-4e74-98d3-8ef0514c0463>
2013-03-07 21:43:57 +0000

    FIXED: WiiStreams: WiiFileStream writing

--- trunk/platform-specific/wii/streams/WiiFileStream.cpp
+++ trunk/platform-specific/wii/streams/WiiFileStream.cpp
@@ -25,7 +25,9 @@ largesize_t WiiFileStream::read(void* buf, largesize_t size)
 largesize_t WiiFileStream::write(const void* buf, largesize_t size)
 {
 	size = min(m_size - m_position, size);
-	return m_image.wii_write_data(m_partition, m_offset + m_position, &MemoryStream(buf, static_cast<ptrdiff_t>(size)), size) ? size : 0;
+	const largesize_t written = m_image.wii_write_data(m_partition, m_offset + m_position, &MemoryStream(buf, static_cast<ptrdiff_t>(size)), size) ? size : 0;
+	m_position += written;
+	return written;
 }
 
 offset_t WiiFileStream::seek(offset_t offset, Stream::SeekOrigin origin)
