06f2020e145098ff0a0895009b2276f5c1635966
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2013-03-07 21:46:35 +0000

    SHSM: PatcherLib: Added ui.arc replacing and checking; added mergemap loading

--- game-specific/Shattered Memories/PatcherLib/PatcherProcessor.cpp
+++ game-specific/Shattered Memories/PatcherLib/PatcherProcessor.cpp
@@ -40,11 +40,37 @@ bool PatcherProcessor::openImage(const QString& path)
 	return true;
 }
 
+static Archive::MergeMap loadMergeMap(const QString& path)
+{
+	Archive::MergeMap map;
+
+	QtFileStream stream(path, QIODevice::ReadOnly);
+	if (stream.opened())
+	{
+		if ((stream.size() % 8) != 0)
+		{
+			DLOG << "WARNING: Invalid merge map size!";
+		}
+
+		while (!stream.atEnd())
+		{
+			const quint32 sourceHash = stream.readUInt();
+			const quint32 destHash = stream.readUInt();
+			map[sourceHash] = destHash;
+		}
+
+	}
+
+	return map;
+}
+
 bool PatcherProcessor::rebuildArchives(const QString& outPath, const QStringList& resourcesPaths, const ExecutableInfo& executableInfo)
 {
 	const CompoundProgressNotifier::ProgressGuard guard(m_progressNotifier);
+	const QDir outDir(outPath);
+	const QDir resDir(resourcesPaths.first());
 
-	TextureDatabase textureDB = TextureDatabase::fromCSV(QDir(resourcesPaths.first()).absoluteFilePath("textures.csv"));
+	TextureDatabase textureDB = TextureDatabase::fromCSV(resDir.absoluteFilePath("textures.csv"));
 	if (textureDB.isNull())
 	{
 		m_errorCode = RebuildArchives_UnableToLoadTextureDatabase;
@@ -52,27 +78,16 @@ bool PatcherProcessor::rebuildArchives(const QString& outPath, const QStringList
 		return false;
 	}
 
+	Archive::MergeMap mergeMap;
+	const QString mergeMapFilename = "mergemap.bin";
+	if (resDir.exists(mergeMapFilename))
+	{
+		mergeMap = loadMergeMap(resDir.absoluteFilePath(mergeMapFilename));
+	}
+
+
 	PatcherDirectoriesFileSource arcDirectoriesFileSource(resourcesPaths);
 	PatcherTexturesFileSource arcTexturesFileSource(&arcDirectoriesFileSource, resourcesPaths.first(), textureDB);
-
-	Archive::MergeMap mergeMap;
-	// Strings.Fre
-	mergeMap[0x2C238179] = 0x2C238264;
-	// Strings.Ita
-	mergeMap[0x2C2352B4] = 0x2C238264;
-	// Strings.Ger
-	mergeMap[0x2C239BD8] = 0x2C238264;
-	// Strings.Spa
-	mergeMap[0x2C23CD2A] = 0x2C238264;
-	// Strings.Jap
-	mergeMap[0x2C234C13] = 0x2C238264;
-	
-	// BootStrings.Fre
-	mergeMap[0x7EFC8F0F] = 0x7EFCA512;
-	// BootStrings.Ita
-	mergeMap[0x7EFC92C2] = 0x7EFCA512;
-	// BootStrings.Ger
-	mergeMap[0x7EFC8DEE] = 0x7EFCA512;
 
 	DLOG << "Rebuilding UI.arc...";
 	{
@@ -114,7 +129,7 @@ bool PatcherProcessor::rebuildArchives(const QString& outPath, const QStringList
 
 		uiArc.addProgressListener(&listener);
 
-		const QString filename = QDir(outPath).absoluteFilePath("ui.arc");
+		const QString filename = outDir.absoluteFilePath("ui.arc");
 		if (!uiArc.rebuild(filename.toStdWString(), arcTexturesFileSource, mergeMap))
 		{
 			m_errorCode = RebuildArchives_UnableToRebuildArchive;
@@ -168,7 +183,7 @@ bool PatcherProcessor::rebuildArchives(const QString& outPath, const QStringList
 		}
 
 		bootArc.addProgressListener(&listener);
-		const QString filename = QDir(outPath).absoluteFilePath("boot.arc");
+		const QString filename = outDir.absoluteFilePath("boot.arc");
 		if (!bootArc.rebuild(filename.toStdWString(), arcTexturesFileSource, mergeMap))
 		{
 			m_errorCode = RebuildArchives_UnableToRebuildArchive;
@@ -199,7 +214,7 @@ bool PatcherProcessor::rebuildArchives(const QString& outPath, const QStringList
 		}
 
 		arc.addProgressListener(&listener);
-		const QString filename = QDir(outPath).absoluteFilePath("data.arc");
+		const QString filename = outDir.absoluteFilePath("data.arc");
 		if (!arc.rebuild(filename.toStdWString(), arcTexturesFileSource, mergeMap))
 		{
 			m_errorCode = RebuildArchives_UnableToRebuildArchive;
@@ -238,7 +253,7 @@ bool PatcherProcessor::replaceArchives(const QString& arcPath, const ExecutableI
 		return false;
 	}
 
-	// Boot.arc
+	DLOG << "Replacing boot.arc...";
 	{
 		executableStream->seek(executableInfo.bootArcOffset, Stream::seekSet);
 		const EmbededResourceInfo info = EmbededResourceInfo::parse(executableStream.get());
@@ -275,9 +290,60 @@ bool PatcherProcessor::replaceArchives(const QString& arcPath, const ExecutableI
 		executableStream->write32(stream.size());
 	}
 
+	DLOG << "Replacing ui.arc...";
+	{
+		auto_ptr<Stream> pakStream(m_image.openFile("igc.arc", Stream::modeReadWrite));
+		if (pakStream.get() == NULL)
+		{
+			m_errorCode = ReplaceArchives_UnableToOpenArcForReplace;
+			m_errorData = "igc.arc";
+			return false;
+		}
+
+		Archive arc(pakStream.get());
+		if (!arc.open())
+		{
+			m_errorCode = ReplaceArchives_UnableToParseArc;
+			m_errorData = "igc.arc";
+			return false;
+		}
+
+		shared_ptr<Stream> uiArcStream = arc.openFile("ui.arc");
+		if (uiArcStream.get() == NULL)
+		{
+			m_errorCode = ReplaceArchives_UnableToOpenArcForReplace;
+			m_errorData = "ui.arc";
+			return false;
+		}
+
+		QtFileStream stream(dir.absoluteFilePath("ui.arc"), QIODevice::ReadOnly);
+		if (!stream.opened())
+		{
+			m_errorCode = ReplaceArchives_UnableToOpenInputPak;
+			m_errorData = "ui.arc";
+			return false;
+		}
+
+		if (stream.size() > uiArcStream->size())
+		{
+			DLOG << "Input ui.arc too big: " << stream.size() << " bytes / " << uiArcStream->size() << " bytes";
+			m_errorCode = ReplaceArchives_InputArcFileTooBig;
+			m_errorData = QString("data.arc;%2;%3").arg(stream.size()).arg(uiArcStream->size());
+			return false;
+		}
+
+		if (uiArcStream->writeStream(&stream, stream.size()) != stream.size())
+		{
+			DLOG << "Unable to write ui.arc!";
+			m_errorCode = ReplaceArchives_UnableToWriteFile;
+			m_errorData = "ui.arc";
+			return false;
+		}
+	}
+
 	executableStream->seek(executableInfo.headersOffset, Stream::seekSet);
 
-	// data.arc
+	DLOG << "Replacing data.arc...";
 	{
 		WiiImageProgressNotifier listener;
 		m_image.setProgressHandler(&listener);
@@ -325,7 +391,12 @@ bool PatcherProcessor::replaceArchives(const QString& arcPath, const ExecutableI
 		}
 
 		stream.seek(0, Stream::seekSet);
-		executableStream->writeStream(&stream, info.contentSize);
+		if (executableStream->writeStream(&stream, info.contentSize) != info.contentSize)
+		{
+			m_errorCode = ReplaceArchives_UnableToWriteFile;
+			m_errorData = "header:data.arc";
+			return false;
+		}
 	}
 
 	return true;
@@ -377,6 +448,48 @@ bool PatcherProcessor::checkArchives(const QString& arcPath, const ExecutableInf
 		{
 			m_errorCode = CheckArchives_ArcsAreNotEqual;
 			m_errorData = "boot.arc";
+			return false;
+		}
+	}
+
+	DLOG << "Checking ui.arc...";
+	{
+		auto_ptr<Stream> pakStream(m_image.openFile("igc.arc", Stream::modeReadWrite));
+		if (pakStream.get() == NULL)
+		{
+			m_errorCode = CheckArchives_UnableToOpenArchive;
+			m_errorData = "igc.arc";
+			return false;
+		}
+
+		Archive arc(pakStream.get());
+		if (!arc.open())
+		{
+			m_errorCode = CheckArchives_UnableToParseArchive;
+			m_errorData = "igc.arc";
+			return false;
+		}
+
+		shared_ptr<Stream> uiArcStream = arc.openFile("ui.arc");
+		if (uiArcStream.get() == NULL)
+		{
+			m_errorCode = CheckArchives_UnableToOpenArchive;
+			m_errorData = "ui.arc";
+			return false;
+		}
+
+		QtFileStream resultArcSream(dir.absoluteFilePath("ui.arc"), QIODevice::ReadOnly);
+		if (!resultArcSream.opened())
+		{
+			m_errorCode = CheckArchives_UnableToOpenResultArchive;
+			m_errorData = "ui.arc";
+			return false;
+		}
+
+		if (!compareStreams(uiArcStream.get(), &resultArcSream, true, 0.04))
+		{
+			m_errorCode = CheckArchives_ArcsAreNotEqual;
+			m_errorData = "ui.arc";
 			return false;
 		}
 	}
