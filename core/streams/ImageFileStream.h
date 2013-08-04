7a49ae1fcecf17b51ec894cce23d350142091164
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2013-08-04 11:35:25 +0000

    CORE: Streams: Refactoring

--- core/streams/ImageFileStream.h
+++ core/streams/ImageFileStream.h
@@ -21,6 +21,7 @@ public:
 	virtual offset_t seek(offset_t offset, SeekOrigin origin) override;
 	virtual offset_t position() const override;
 	virtual offset_t size() const override;
+	virtual bool atEnd() const override;
 
 private:
 	Stream* m_stream;
