022cae6550b524f901cdd920a4582dacff501686
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2013-08-31 13:29:31 +0000

    SHSM: Patcher: Fixed PatcherSpec.h example

--- game-specific/Shattered Memories/Patcher/PatchSpec.h
+++ game-specific/Shattered Memories/Patcher/PatchSpec.h
@@ -5,3 +5,4 @@
 #define GAME_ABBR   "SHSM"
 #define TOPIC_URL   "http://consolgames.ru/forum/"
 #define PROJECT_URL "http://consolgames.ru/translations/Silent_Hill:_Shattered_Memories"
+#define PLATFORMS   "wii"
