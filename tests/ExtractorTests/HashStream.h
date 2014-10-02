a90f86923fe76eeaf35003f48a0e47a9c18f552c
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2014-10-02 22:26:01 +0000

    Fixed HashStream

--- tests/ExtractorTests/HashStream.h
+++ tests/ExtractorTests/HashStream.h
@@ -14,10 +14,10 @@ public:
 	virtual largesize_t read(void*, largesize_t) override {return 0;}
 	virtual largesize_t write(const void* buf, largesize_t size);
 	virtual offset_t seek(offset_t, SeekOrigin) override {return 0;}
-	virtual offset_t tell() const override {return 0;}
+	virtual offset_t position() const override {return 0;}
 	virtual void flush() override {}
 	virtual offset_t size() const;
-
+	virtual bool atEnd() const override {return false;}
 
 protected:
 	QCryptographicHash m_hash;
