2a038e381830f3ed89a43bc52e994cdb0a6c30e9
horror_x <horror_x@02863142-2508-4e74-98d3-8ef0514c0463>
2014-10-18 14:48:30 +0000

    SH: FilesExtractor total refactoring

--- trunk/game-specific/Silent Hill/FilesExtractor/Silent.h
+++ trunk/game-specific/Silent Hill/FilesExtractor/Silent.h
@@ -1,21 +1,3 @@
-#ifndef SILENT_H_INCLUDED
-#define SILENT_H_INCLUDED
+#pragma once
 
-struct FileRec
-{
-    unsigned int a; // File size and offset in RAM (encrypted by very very long and boring procedure)
-    unsigned int b; // First part of name and directory index
-    unsigned int c; // Second part of name and exstention index
-};
-
-inline unsigned int decodeSize(unsigned int v)
-{
-    unsigned int lba = ((v >> 0x13) & 0xFFFF) << 8;
-    return lba;
-}
-
-int DetectRegion(void* data);
-int ExtractFiles(FileRec* rec, int count, const char *in_file, const char *out_dir, int ver = 0); // 0 = EUR, 1 - USA
-int ExtractFiles(const char* exe_file, const char *in_file, const char *out_dir);
-
-#endif // SILENT_H_INCLUDED
+bool extractFiles(const char* executablePath, const char *silentPath, const char *outputDirectory);
