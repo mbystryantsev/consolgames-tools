390904a0986b000a0818fc03b1364e97e1700608
Mikhail Bystryantsev <mbystryantsev@users.noreply.github.com>
2014-10-19 12:04:18 +0000

    SH: FilesExtractor: Fixed some output

--- game-specific/Silent Hill/FilesExtractor/main.cpp
+++ game-specific/Silent Hill/FilesExtractor/main.cpp
@@ -13,18 +13,16 @@ int main(int argc, char *argv[])
 {
     if (argc < 4)
     {
-        printf(
+        std::cout <<
 			"Silent Hill Files Extractor by horror_x\n"
 			"http://consolgames.ru/\n"
 			"Usage:\n"
-			"    SHExtract.exe <Executable> <SILENT> <OutDir>\n\n"
-        );
+			"    SHExtract.exe <Executable> <SILENT> <OutDir>\n\n";
 		printSupportedVersions();
         return -2;
     }
 
-    printf("Extracting...\n");
     const bool result = extractFiles(argv[1], argv[2], argv[3]);
-    printf(result ? "Error!\n" : "Done!\n");
+    std::cout << (result ? "Done!" : "Error!") << std::endl;
     return result ? 0 : -1;
 }
