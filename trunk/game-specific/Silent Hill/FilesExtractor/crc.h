476521a0e5b521d5433444936dfedf9d59002d0a
horror_x <horror_x@02863142-2508-4e74-98d3-8ef0514c0463>
2014-11-01 15:51:49 +0000

    SH: FilesExtractor: size_t -> uint32_t

--- trunk/game-specific/Silent Hill/FilesExtractor/crc.h
+++ trunk/game-specific/Silent Hill/FilesExtractor/crc.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "Common.h"
 
-uint16_t crc16(const void* buf, size_t len, uint16_t crc = 0);
-uint32_t crc32(const void* buf, size_t len, uint32_t crc = 0);
+uint16_t crc16(const void* buf, uint32_t len, uint16_t crc = 0);
+uint32_t crc32(const void* buf, uint32_t len, uint32_t crc = 0);
